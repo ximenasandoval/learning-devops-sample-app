8f4249d84558abfc1fe33e8b7ad2e56729e6fc46
Ximena Sandoval <ximena.sandoval.dh@gmail.com>
2023-05-31 11:49:47 -0700

    Tweak deploy script for Jenkins

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,11 +5,13 @@ date=$(date '+%Y-%m-%d-%H-%M-%S')
 tag=deploy-$date
 docker build -f app/Dockerfile -t $tag app/
 
-# Load env variables
-export $(cat deployment/.env | xargs)
-
-# Unset default AWs profile (will delete later)
-unset AWS_PROFILE
+# If we don't have env variables, we need to set them
+if  [[ -z ${AWS_ACCOUNT_ID+x} ||  -z ${AWS_SECRET_ACCESS_KEY+x} || -z ${AWS_ACCESS_KEY_ID+x} ]]; then 
+    echo "No env variables, loading from file"
+    export $(cat deployment/.env | xargs)
+    # Unset default AWs profile (will delete later)
+    unset AWS_PROFILE
+fi
 
 # Push image to ECR repository
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS \
